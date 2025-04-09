bac34b76d0e28869300bdbd64c5fbfd46935d7e9
Adam <adam.fafera@edu.uekat.pl>
2025-04-09 11:04:40 +0200

    Solution ✅: TwoSum brute force

---
+++ TwoSum/TwoSum.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number) {
+    // // Brute Force
+    // let array: number[] = [];
+    //
+    // for(let i : number=0; i<nums.length; i++){
+    //     for (let j : number=i+1; j<nums.length; j++){
+    //         if (nums[i] + nums[j] == target){
+    //             array.push(i)
+    //             array.push(j)
+    //             return array;
+    //         }
+    //     }
+    // }
+
+}
+
+console.log(twoSum([1,3,4,5], 7))
